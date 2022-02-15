5e554f3e4b78e577467a55b3c961416e7a8a3274
Mustafa <mustafa_sahinci@hotmail.com>
2022-02-15 02:50:23 +0000

    aa

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,6 +75,8 @@ const cards2 = document.querySelectorAll('.flip-card-hard');
 let hasFlippedCard = false;
 let lockBoard = false;
 let firstCard, secondCard;
+let firstCard2, secondCard1;
+let firstCard1, secondCard2;
 
 //shuffle the cards
 function shuffle() {
@@ -82,19 +84,19 @@ function shuffle() {
     let randomPos = Math.floor(Math.random() * 8);
     card.style.order = randomPos;
   });
-  cards1.forEach(card => {
+  cards1.forEach(card1 => {
     let randomPos = Math.floor(Math.random() * 12);
-    card.style.order = randomPos;
+    card1.style.order = randomPos;
   });
-  cards2.forEach(card => {
+  cards2.forEach(card2 => {
     let randomPos = Math.floor(Math.random() * 16);
-    card.style.order = randomPos;
+    card2.style.order = randomPos;
   });
 };
 
 cards.forEach(card => card.addEventListener('click', flipCard));
-cards1.forEach(card => card.addEventListener('click', flipCard));
-cards2.forEach(card => card.addEventListener('click', flipCard));
+cards1.forEach(card1 => card1.addEventListener('click', flipCard));
+cards2.forEach(card2 => card2.addEventListener('click', flipCard));
 
 shuffle();
 
@@ -102,16 +104,22 @@ shuffle();
 function flipCard() {
   if (lockBoard) return;
   if (this === firstCard) return;
+  if (this === firstCard1) return;
+  if (this === firstCard2) return;
   startTimer();
   this.classList.add('flip');
 
   if (!hasFlippedCard) {
     hasFlippedCard = true;
     firstCard = this;
+    firstCard1 = this;
+    firstCard2 = this;
   
     return;
   }
   secondCard = this;
+  secondCard1 = this;
+  secondCard2 = this;
   checkForMatch();
   moves()
 }
@@ -127,8 +135,12 @@ let cardCorrect = 0;
 
 function checkForMatch() {
   let isMatch = firstCard.dataset.image === secondCard.dataset.image;
+  let isMatch1 = firstCard1.dataset.image === secondCard1.dataset.image;
+  let isMatch2 = firstCard2.dataset.image === secondCard2.dataset.image;
 
   isMatch ? disableCards() : unflipCards();
+  isMatch1 ? disableCards() : unflipCards();
+  isMatch2 ? disableCards() : unflipCards();
 }
 
 function disableCards() {
@@ -140,7 +152,35 @@ function disableCards() {
     setTimeout(function(){
     alert("Congratulations! You found all the pairs!"); 
   }, 1000)
-};
+}; 
+  
+  resetBoard();
+}
+
+function disableCards() {
+  firstCard1.removeEventListener('click', flipCard);
+  secondCard1.removeEventListener('click', flipCard);
+  cardCorrect++
+  console.log(cardCorrect)
+  if (cardCorrect === 6) {
+    setTimeout(function(){
+    alert("Congratulations! You found all the pairs!"); 
+  }, 1000)
+}; 
+  
+  resetBoard();
+}
+
+function disableCards() {
+  firstCard2.removeEventListener('click', flipCard);
+  secondCard2.removeEventListener('click', flipCard);
+  cardCorrect++
+  console.log(cardCorrect)
+  if (cardCorrect === 8) {
+    setTimeout(function(){
+    alert("Congratulations! You found all the pairs!"); 
+  }, 1000)
+}; 
   
   resetBoard();
 }
@@ -151,6 +191,10 @@ function unflipCards() {
   setTimeout(() => {
     firstCard.classList.remove('flip');
     secondCard.classList.remove('flip');
+    firstCard1.classList.remove('flip');
+    secondCard1.classList.remove('flip');
+    firstCard2.classList.remove('flip');
+    secondCard2.classList.remove('flip');
 
     resetBoard();
   }, 1000);
@@ -159,6 +203,8 @@ function unflipCards() {
 function resetBoard() {
   [hasFlippedCard, lockBoard] = [false, false];
   [firstCard, secondCard] = [null, null];
+  [firstCard1, secondCard1] = [null, null];
+  [firstCard2, secondCard2] = [null, null];
 }
 
 
